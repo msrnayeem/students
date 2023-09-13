3a6bc9b68554e3a3ae994e8276e648b6e17ff384
Msr Nayeem <msrnayeem@gmail.com>
2023-09-13 21:25:54 +0600

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentAssignment.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var db = new studentsEntities();
+            var students = db.Student_Tables.ToList();
+
+            ViewBag.StudentData = students;
+
+            return View();
+        }
+    }
+}
